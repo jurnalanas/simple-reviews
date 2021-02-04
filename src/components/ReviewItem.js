6ce89bbdfd11000883fad22fcf288ccfb1944991
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2021-02-04 20:57:02 +0700

    added review item page

---
+++ src/components/ReviewItem.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const ReviewItem = props => {
+
+  return (
+    <div className="w-1/2 p-3 m-auto bg-white shadow overflow-hidden sm:rounded-lg">
+      <div className="px-4 py-5 sm:px-6">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+          "{props.reviewTitle}"
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+          {props.profileName}
+        </p>
+      </div>
+      <div className="border-t border-gray-200">
+        <dl>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              Review Rating
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {props.reviewRating}
+            </dd>
+          </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              Product
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {props.product}
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              Helpful Count
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {props.helpfulCount}
+            </dd>
+          </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              Total Comments
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {props.totalComments}
+            </dd>
+          </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              Review Country
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {props.reviewCountry}
+            </dd>
+          </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              Reviewed At
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {props.reviewedAt}
+            </dd>
+          </div>
+          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              Company
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {props.productCompany}
+            </dd>
+          </div>
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-sm font-medium text-gray-500">
+              URL
+            </dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <a href={props.url} alt='product url'>
+                {props.url}
+              </a>
+            </dd>
+          </div>
+          <Link to={`/`} className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <button className="bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded text-sm text-white mr-1">
+              Go to Homepage
+            </button>
+          </Link>
+        </dl>
+      </div>
+    </div>
+  )
+}
+
+ReviewItem.propTypes = {
+  profileName: PropTypes.string,
+  reviewText: PropTypes.string,
+  reviewRating: PropTypes.string,
+  id: PropTypes.number,
+  reviewTitle: PropTypes.string,
+  product: PropTypes.string,
+  helpfulCount: PropTypes.string,
+  totalComments: PropTypes.string,
+  url: PropTypes.string,
+  reviewCountry: PropTypes.string,
+  reviewedAt: PropTypes.string,
+  productCompany: PropTypes.string
+};
+
+export default ReviewItem;
--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import ReviewItem from '../components/ReviewItem';
 import LoadingSpinner from '../shared/components/UIElements/LoadingSpinner';
 import ErrorModal from '../shared/components/UIElements/ErrorModal';
 import { useHttpClient } from '../shared/hooks/http-hook';
@@ -11,6 +12,7 @@ const Reviews = () => {
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   useEffect(() => {
+    // we use array index as id because there is a unique id property on Review Item response API
     const fetchProducts = async () => {
       try {
         const responseData = await sendRequest(process.env.REACT_APP_API_HOST);
@@ -23,16 +25,31 @@ const Reviews = () => {
   }, [reviewId, sendRequest]);
 
   if (dataFetched) {
-    console.log({reviewItem})
     return (
-      <>
-        {isLoading && <LoadingSpinner asOverlay />}
-        {!isLoading && dataFetched &&
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            {reviewItem.product}
+    <div className="bg-gray-200 pt-10 h-screen">
+      <div className="container mx-auto">
+        <div className="px-4 mb-16 text-gray-800 leading-normal">
+          <div className="px-4 mb-16 text-gray-800 leading-normal">
+            {isLoading && <LoadingSpinner asOverlay />}
+            {!isLoading && dataFetched &&
+              <ReviewItem
+                profileName={reviewItem.profile_name}
+                reviewText={reviewItem.review_text}
+                reviewRating={reviewItem.review_rating}
+                reviewTitle={reviewItem.review_title}
+                product={reviewItem.product}
+                helpfulCount={reviewItem.helpful_count}
+                totalComments={reviewItem.total_comments}
+                url={reviewItem.url}
+                reviewCountry={reviewItem.review_country}
+                reviewedAt={reviewItem.reviewed_at}
+                productCompany={reviewItem.product_company}
+              />
+            }
           </div>
-        }
-      </>
+        </div>
+      </div>
+    </div>
     );
   } else {
     return (
