880c08c5795ef5901fb17544fe62a4b9126cfcc8
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2021-02-04 17:06:19 +0700

    incorporate API call

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Contents from "./components/Contents";
 function App() {
   return (
     <>
-      <div className="bg-gray-200 h-screen">
+      <div className="bg-gray-200">
         <Navbar />
         <div className="container mx-auto">
           <div className="px-4 mb-16 text-gray-800 leading-normal">
--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReviewCard from './ReviewCard';
+import LoadingSpinner from '../shared/components/UIElements/LoadingSpinner';
+import ErrorModal from '../shared/components/UIElements/ErrorModal';
+import { useHttpClient } from '../shared/hooks/http-hook';
 
 const Contents = props => {
-  // if (props.items.length === 0) {
-  //   return (
-  //     <div>
-  //       <h2>No Reviews found</h2>
-  //     </div>
-  //   )
-  // }
+  const [dataFetched, setFetchData] = useState(false);
+  const [reviewsData, setReviewsData] = useState([]);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      <ReviewCard
-        profileName='profileName'
-        reviewRating='5'
-        reviewTitle='Title'
-      />
-    </div>
-  )
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const responseData = await sendRequest(process.env.REACT_APP_API_HOST);
+        const dataList = await responseData['data'];
+        setFetchData(true);
+        setReviewsData(dataList);
+      } catch (err) {}
+    };
+    fetchProducts();
+  }, [sendRequest]);
+
+  if (dataFetched) {
+    console.log({reviewsData})
+    return (
+      <>
+        {isLoading && <LoadingSpinner asOverlay />}
+        {!isLoading && dataFetched &&
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            {reviewsData.map(item => {
+              const reviewRating = parseFloat(item.review_rating.split(' ')[0]);
+              return (
+                <ReviewCard
+                  profileName={item.profile_name}
+                  reviewRating={reviewRating}
+                  reviewTitle={item.review_title}
+                />
+              )
+            })};
+          </div>
+        }
+      </>
+    );
+  } else {
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {isLoading && <LoadingSpinner asOverlay />}
+        {!isLoading &&
+          <div className="container max-w-xl mx-auto">
+            <ErrorModal error={error} onClear={clearError} />
+          </div>
+        }
+      </div>
+    );
+  }
 }
 
 export default Contents;
