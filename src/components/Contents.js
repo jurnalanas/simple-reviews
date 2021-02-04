834b793d818d4bf67ddfa745f9848002b2a1839e
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2021-02-04 15:55:25 +0700

    setup env

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -3,7 +3,7 @@ import CardsLarge from './CardsLarge';
 
 const Contents = () => {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
       <CardsLarge/>
       <CardsLarge/>
       <CardsLarge/>
