8950939e707738677f2d81f12b4c54440146cf30
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2021-02-04 17:21:23 +0700

    incorporate review item call

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -1,21 +1,51 @@
-import React from 'react';
-import Navbar from "../components/Navbar";
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Contents from "../components/Contents";
+import LoadingSpinner from '../shared/components/UIElements/LoadingSpinner';
+import ErrorModal from '../shared/components/UIElements/ErrorModal';
+import { useHttpClient } from '../shared/hooks/http-hook';
 
 const Reviews = () => {
   const reviewId = useParams().reviewId;
-  console.log({reviewId})
-  return (
-    <div className="bg-gray-200">
-      <Navbar />
-      <div className="container mx-auto">
-        <div className="px-4 mb-16 text-gray-800 leading-normal">
-          <Contents />
-        </div>
+  const [dataFetched, setFetchData] = useState(false);
+  const [reviewItem, setReviewItem] = useState({});
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const responseData = await sendRequest(process.env.REACT_APP_API_HOST);
+        const dataList = await responseData['data'];
+        setFetchData(true);
+        setReviewItem(dataList[reviewId]);
+      } catch (err) {}
+    };
+    fetchProducts();
+  }, [reviewId, sendRequest]);
+
+  if (dataFetched) {
+    console.log({reviewItem})
+    return (
+      <>
+        {isLoading && <LoadingSpinner asOverlay />}
+        {!isLoading && dataFetched &&
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            {reviewItem.product}
+          </div>
+        }
+      </>
+    );
+  } else {
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {isLoading && <LoadingSpinner asOverlay />}
+        {!isLoading &&
+          <div className="container max-w-xl mx-auto">
+            <ErrorModal error={error} onClear={clearError} />
+          </div>
+        }
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Reviews;
