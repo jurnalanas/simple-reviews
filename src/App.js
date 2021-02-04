e1663a5aac7e9222e4cf2bdce18c9c00436768be
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2021-02-04 15:34:35 +0700

    added React and Jest-specific ESLint config

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div>
       <div className="container w-full mx-auto pt-20">
         <div className="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
+        hmm
         </div>
       </div>
     </div>
