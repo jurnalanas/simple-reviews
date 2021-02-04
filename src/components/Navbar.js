b1d145eec0cb258d607668a5d8e99d02c9721d06
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2021-02-04 17:24:15 +0700

    added back button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
 const Navbar = () => {
+  let history = useHistory();
   return (
-    <nav className="container mx-auto pt-4">
+    <nav className="container mx-auto pt-4 mb-5">
       <div className="flex px-7">
+          <button className="mx-2 px-1 m-auto" onClick={() => history.goBack()}>
+            Back
+          </button>
         <input type="search" className="w-full bg-purple-white shadow rounded border-0 p-2" placeholder="Search by name..." />
       </div>
     </nav>
